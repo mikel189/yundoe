420ae8feac3cbd3e0aee502a5036aebc831f63f9
Ibrahim <ejallow189@gmail.com>
2020-09-02 03:42:57 +0000

    update script names

---
+++ app.py
@@ -0,0 +1,16 @@
+import pymongo
+import numpy as np
+import pandas as pd
+from pymongo import MongoClient
+
+mongo_uri = 'mongodb://localhost:27017/'
+
+def format_predictions(df):
+    json_df = df.to_json(date_format = 'iso')
+    return json_df
+
+def insert_predictions_to_db(json_df):
+    client = MongoClient(mongo_uri)
+    collection = client.lasperrlive
+    post_id = collection.insert_one({json_df}).inserted_id
+    return post_id
---
+++ models.py
@@ -0,0 +1,26 @@
+import time
+import numpy as np
+import pandas as pd
+
+import datetime
+from datetime import timedelta
+
+from fbprophet import Prophet
+
+import sanitize
+
+def build_model():
+    df = sanitize.preprocess_data()
+    model = Prophet()
+    model.fit(df)
+    future = model.make_future_dataframe(periods = 4, freq = 'W')
+    forecast = model.predict(future)
+    return forecast
+
+def predictions():
+    forecast = build_model()
+    forecast_df = forecast[['ds', 'yhat_upper', 'yhat', 'yhat_lower']]
+    return forecast_df.tail()
+
+
+

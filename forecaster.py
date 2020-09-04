a72fd344a2a4722fd11d582fa2db5492180b4192
Ibrahim <ejallow189@gmail.com>
2020-09-04 18:19:11 +0000

    set up env variables and associated functionalities

--- forecaster.py
+++ forecaster.py
@@ -10,15 +10,18 @@ def build_model():
     forecast = model.predict(future)
     return forecast
 
+
 def predictions():
     forecast = build_model()
     forecast_df = forecast[['ds', 'yhat_upper', 'yhat', 'yhat_lower']]
     return forecast_df.tail(1)
 
+
 def get_train_model():
     prediction_df = predictions()
     print(prediction_df)
     return prediction_df
+
 
 if __name__ == '__main__':
     get_train_model()
--- sanitize.py
+++ sanitize.py
@@ -1,43 +1,45 @@
 import pymongo
 # import numpy as np
 import pandas as pd
+from os import environ as env
 from pymongo import MongoClient
+from dotenv import load_dotenv, find_dotenv
 
-mongo_uri = 'mongodb://localhost:27017/'
-client = MongoClient(mongo_uri)
 
-# client.list_database_names()
-# client.list_collection_names()
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
+mongo_uri = env.get("MONGO_URI")
 
-# db = client['lasperr-live']
-# collection = db['payments']
 
+def connect_to_db():
+    client = MongoClient(host=mongo_uri)
+    db = client.lasperrlive
+    return db
 
 def get_payments_collection():
-    client = MongoClient(mongo_uri)
-    db = client.lasperrlive
+    db = connect_to_db()
     payments_collection = db.payments
     return payments_collection
 
 
 def read_from_db():
     payments_collection = get_payments_collection()
-    payments_collection_data = payments_collection.find().sort(('date', pymongo.DESCENDING))[:100]
+    payments_collection_data = payments_collection.find({}).sort(('date'))
     payments_df = pd.DataFrame(payments_collection_data)
     return payments_df
 
 
 def load_and_generate_clean_data(df):
-    # raw_df = pd.read_csv('data/lassperr_payments.csv')
     payments_df = df.copy()
-    # df = raw_df.copy()
-    df[['date', 'time']] = payments_df['date'].str.split('T', expand = True)
-    clean_df = df[['date', 'amountInGMD']]
-    clean_df = clean_df.rename(columns={'date': 'ds', 'amountInGMD': 'y'}, inplace = True)
+    payments_df[['date', 'time']] = payments_df['date'].astype(str).str.split(' ', expand = True)
+    clean_df = payments_df[['date', 'amountInGMD']]
+    clean_df = clean_df.rename(columns={'date': 'ds', 'amountInGMD': 'y'})
     return clean_df
 
 
-def format_and_sort_date_values(df):
+def format_and_sort_date_values(clean_df):
+    df = clean_df.copy()
     df['ds'] = pd.to_datetime(df['ds'])
     sorted_df_dates = df.sort_values('ds', ascending=True)
     return sorted_df_dates
@@ -47,8 +49,10 @@ def preprocess_data():
     df = read_from_db()
     clean_data = load_and_generate_clean_data(df)
     sanitized_df = format_and_sort_date_values(clean_data)
+    print('sanitized payments df', sanitized_df)
     return sanitized_df
 
 
 if __name__ == '__main__':
+    print('preprocessing data......')
     preprocess_data()

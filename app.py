37e789e672557694af33c44848caf7afb8e5dcaf
Ibrahim <ejallow189@gmail.com>
2020-09-07 00:30:27 +0000

    db collections and field names set

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 from pymongo import MongoClient
-from sanitize import get_payments_collection
+from sanitize import connect_to_db
+# from sanitize import get_payments_collection
+from forecaster import get_train_model
 
-def format_predictions(df):
-    json_df = df.to_json(date_format='iso')
-    return json_df
+def get_payments_collection():
+    db = connect_to_db()
+    payments_collection = db.payments
+    return payments_collection
 
 
-def insert_predictions_to_db(json_df):
+def format_predictions():
+    model_df = get_train_model()
+    model_in_dict = model_df.to_dict()
+    predictions = {'predictions': model_in_dict}  
+    return predictions      
+
+def insert_predictions_to_db():
+    model_predictions = format_predictions()
+    print('model in dict', model_predictions)
     payments_collection = get_payments_collection()
-    post_id = payments_collection.insert_one({json_df}).inserted_id
+    post_id = payments_collection.insert_one(model_predictions).inserted_id
     return post_id
+
+
+if __name__ == '__main__':
+    insert_predictions_to_db()

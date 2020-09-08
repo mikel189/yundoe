981e83740df17037f7fcad5d42e8cfcf35ea6566
Ibrahim <ejallow189@gmail.com>
2020-09-08 13:19:10 +0000

    bug fix

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from pymongo import MongoClient
 from sanitize import connect_to_db
 from forecaster import get_train_model
 
-def get_payments_collection():
+def get_forecast_collection():
     db = connect_to_db()
-    payments_collection = db.payments
-    return payments_collection
+    forecast_collection = db.forecast
+    return forecast_collection
 
 
 def format_predictions():
@@ -16,16 +16,23 @@ def format_predictions():
     for key, val in model_in_dict.items():
         for _, value in val.items():
             prediction_dict[key] = value
+    
+    prediction_dict_keys = list(prediction_dict.keys())
+
+    prediction_dict['date'] = prediction_dict.pop(prediction_dict_keys[0])
+    prediction_dict['upperBound'] = prediction_dict.pop(prediction_dict_keys[1])
+    prediction_dict['amount'] = prediction_dict.pop(prediction_dict_keys[2])
+    prediction_dict['lowerBound'] = prediction_dict.pop(prediction_dict_keys[3])
 
     return prediction_dict
 
 
 def insert_predictions_to_db():
-    model_predictions = format_predictions()
-    print('model in dict', model_predictions)
-    payments_collection = get_payments_collection()
-    post_id = payments_collection.insert_one(model_predictions).inserted_id
-    return post_id
+    prediction_dict = format_predictions()
+    print('model in dict', prediction_dict)
+    payments_collection = get_forecast_collection()
+    post_id = payments_collection.insert_one(prediction_dict).inserted_id
+    print(post_id)
 
 
 if __name__ == '__main__':

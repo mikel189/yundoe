24b6607db74c61bb92ce654b997ba60a89bf074a
Ibrahim <ibrahim@assutech.gm>
2020-09-21 13:22:49 +0000

    update curl for testing

--- test/test_endpoint.py
+++ test/test_endpoint.py
@@ -32,3 +32,10 @@ def access_api():
 if __name__ == '__main__':
     get_token()
     access_api()
+
+
+curl --request POST \
+  --url http://127.0.0.1:5000/api/private \
+  --header 'authorization: Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InhBdWEtenVUT184U1JkbWRCOXFEdiJ9.eyJpc3MiOiJodHRwczovL2Rldi0xbmt4M3Rmei51cy5hdXRoMC5jb20vIiwic3ViIjoiQmg1cms5Z1lVN3ViU3AxZWVYN21BR1ZESHk4VURqUTBAY2xpZW50cyIsImF1ZCI6Inl1bmRvZSIsImlhdCI6MTYwMDY4ODk1MywiZXhwIjoxNjAwNzc1MzUzLCJhenAiOiJCaDVyazlnWVU3dWJTcDFlZVg3bUFHVkRIeThVRGpRMCIsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyJ9.ol4YvEPql6SNrB2dbB2N503Ts25PKYYPz5MJvO8WeNbnf5IqXYQNdrXc2jQR8bgmIm7Y-wBKDwrIZ1I9sQrFRG4NWC-vWyzFBjBb4pSYoFR6WZrQzchSuz6UQ2ZNifIiq8NQW_3GLRR3J_GU3vqBrDKUfYzkx4Whlot5X-KZkqa5Qey3qB3wKdi0yXCTmT7Jav9EiVmWuC_ib46H1hLkfhejaLzPqvA8cJKSA6bcqLhgHeY96h2XI4xS4kXy-PwMT69bFpZHxGCGCkBRsAOYAwueH3R5Rjjlm0MSVZnhlNd9tjhJGyt5-cgINQKPVKqe608V9LeVRO1MHDeMDCL6Wg' \
+  --data '{"estate_id": "1234325", "year": 1994}'
+

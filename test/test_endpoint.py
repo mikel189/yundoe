90737bf93f27cfa2ce8559be02d94e67fed8da15
Ibrahim <ibrahim@assutech.gm>
2020-09-21 14:54:56 +0000

    test bug fix

--- test/test_endpoint.py
+++ test/test_endpoint.py
@@ -1,6 +1,8 @@
 import http.client
+import json
+import requests
 
-def get_token():
+def get_access_token():
     conn = http.client.HTTPSConnection("dev-1nkx3tfz.us.auth0.com")
 
     payload = "{\"client_id\":\"Bh5rk9gYU7ubSp1eeX7mAGVDHy8UDjQ0\",\"client_secret\":\"0mpzhJp3OgI0dBfP61cKpIXFK2KgH6bqckf2dLpM64TMEwuCvHe6y60k8VE5wVnc\",\"audience\":\"yundoe\",\"grant_type\":\"client_credentials\"}"
@@ -12,30 +14,33 @@ def get_token():
     res = conn.getresponse()
     data = res.read()
 
-    print(data.decode("utf-8"))
+    return data.decode("utf-8")
+    # print(data.decode("utf-8"))
 
 
 def access_api():
+    token_dict = json.loads(get_access_token())
+    access_token = token_dict['access_token']
 
-    conn = http.client.HTTPConnection("http://127.0.0.1:5000")
+    url = "http://127.0.0.1:5000/api/prediction"
 
-    headers = { 'authorization': "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InhBdWEtenVUT184U1JkbWRCOXFEdiJ9.eyJpc3MiOiJodHRwczovL2Rldi0xbmt4M3Rmei51cy5hdXRoMC5jb20vIiwic3ViIjoiQmg1cms5Z1lVN3ViU3AxZWVYN21BR1ZESHk4VURqUTBAY2xpZW50cyIsImF1ZCI6Inl1bmRvZSIsImlhdCI6MTYwMDY4ODk1MywiZXhwIjoxNjAwNzc1MzUzLCJhenAiOiJCaDVyazlnWVU3dWJTcDFlZVg3bUFHVkRIeThVRGpRMCIsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyJ9.ol4YvEPql6SNrB2dbB2N503Ts25PKYYPz5MJvO8WeNbnf5IqXYQNdrXc2jQR8bgmIm7Y-wBKDwrIZ1I9sQrFRG4NWC-vWyzFBjBb4pSYoFR6WZrQzchSuz6UQ2ZNifIiq8NQW_3GLRR3J_GU3vqBrDKUfYzkx4Whlot5X-KZkqa5Qey3qB3wKdi0yXCTmT7Jav9EiVmWuC_ib46H1hLkfhejaLzPqvA8cJKSA6bcqLhgHeY96h2XI4xS4kXy-PwMT69bFpZHxGCGCkBRsAOYAwueH3R5Rjjlm0MSVZnhlNd9tjhJGyt5-cgINQKPVKqe608V9LeVRO1MHDeMDCL6Wg" }
+    headers = { 'authorization': f"Bearer {access_token}" }
+    body = '{"estate_id": "1234325", "year": 1994}'
+    print('this is headers', headers)
 
-    conn.request("GET", "/api/private", headers=headers)
+    response = requests.post(url, data=body, headers = headers)
 
-    res = conn.getresponse()
-    data = res.read()
+    print(response.text)
 
-    print(data.decode("utf-8"))
 
 
 if __name__ == '__main__':
-    get_token()
+    # get_token()
     access_api()
 
 
-curl --request POST \
-  --url http://127.0.0.1:5000/api/private \
-  --header 'authorization: Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InhBdWEtenVUT184U1JkbWRCOXFEdiJ9.eyJpc3MiOiJodHRwczovL2Rldi0xbmt4M3Rmei51cy5hdXRoMC5jb20vIiwic3ViIjoiQmg1cms5Z1lVN3ViU3AxZWVYN21BR1ZESHk4VURqUTBAY2xpZW50cyIsImF1ZCI6Inl1bmRvZSIsImlhdCI6MTYwMDY4ODk1MywiZXhwIjoxNjAwNzc1MzUzLCJhenAiOiJCaDVyazlnWVU3dWJTcDFlZVg3bUFHVkRIeThVRGpRMCIsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyJ9.ol4YvEPql6SNrB2dbB2N503Ts25PKYYPz5MJvO8WeNbnf5IqXYQNdrXc2jQR8bgmIm7Y-wBKDwrIZ1I9sQrFRG4NWC-vWyzFBjBb4pSYoFR6WZrQzchSuz6UQ2ZNifIiq8NQW_3GLRR3J_GU3vqBrDKUfYzkx4Whlot5X-KZkqa5Qey3qB3wKdi0yXCTmT7Jav9EiVmWuC_ib46H1hLkfhejaLzPqvA8cJKSA6bcqLhgHeY96h2XI4xS4kXy-PwMT69bFpZHxGCGCkBRsAOYAwueH3R5Rjjlm0MSVZnhlNd9tjhJGyt5-cgINQKPVKqe608V9LeVRO1MHDeMDCL6Wg' \
-  --data '{"estate_id": "1234325", "year": 1994}'
+# curl --request POST \
+#   --url http://127.0.0.1:5000/api/private \
+#   --header 'authorization: Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InhBdWEtenVUT184U1JkbWRCOXFEdiJ9.eyJpc3MiOiJodHRwczovL2Rldi0xbmt4M3Rmei51cy5hdXRoMC5jb20vIiwic3ViIjoiQmg1cms5Z1lVN3ViU3AxZWVYN21BR1ZESHk4VURqUTBAY2xpZW50cyIsImF1ZCI6Inl1bmRvZSIsImlhdCI6MTYwMDY4ODk1MywiZXhwIjoxNjAwNzc1MzUzLCJhenAiOiJCaDVyazlnWVU3dWJTcDFlZVg3bUFHVkRIeThVRGpRMCIsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyJ9.ol4YvEPql6SNrB2dbB2N503Ts25PKYYPz5MJvO8WeNbnf5IqXYQNdrXc2jQR8bgmIm7Y-wBKDwrIZ1I9sQrFRG4NWC-vWyzFBjBb4pSYoFR6WZrQzchSuz6UQ2ZNifIiq8NQW_3GLRR3J_GU3vqBrDKUfYzkx4Whlot5X-KZkqa5Qey3qB3wKdi0yXCTmT7Jav9EiVmWuC_ib46H1hLkfhejaLzPqvA8cJKSA6bcqLhgHeY96h2XI4xS4kXy-PwMT69bFpZHxGCGCkBRsAOYAwueH3R5Rjjlm0MSVZnhlNd9tjhJGyt5-cgINQKPVKqe608V9LeVRO1MHDeMDCL6Wg' \
+#   --data '{"estate_id": "1234325", "year": 1994}'
 

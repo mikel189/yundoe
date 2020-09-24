039240f76b55fc4b27322b55961d4a89d3a8511e
Ibrahim <ibrahim@assutech.gm>
2020-09-24 11:32:59 +0000

    import error bug fix

--- api/process_request.py
+++ api/process_request.py
@@ -1,9 +1,9 @@
 import sys
 sys.path.append('/home/ibrahim/assutech/Yundoo/')
 
-from api.models import Prediction
+# from app import Prediction
 
 def fetch_forecast_data(estate_id, year):
-    prediction = Prediction.objects.filter(estate_id = estate_id, year=year)
+    prediction = Prediction.objects.filter(estate_id = estate_id, year=year).order_by('-date')
     return prediction
 

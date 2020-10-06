2e64e039de392c08df9db69ee592dbf29d658aab
Ibrahim <ibrahim@assutech.gm>
2020-10-06 14:19:55 +0000

    deleted modules

--- scripts/process_request.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-sys.path.append('/home/ibrahim/assutech/Yundoo/')
-
-def fetch_forecast_data(estate_id, year):
-    from api.models import Prediction
-    
-    prediction = Prediction.objects.filter(estateId=estate_id, year=year).order_by('-createdAt').first()
-    return prediction
